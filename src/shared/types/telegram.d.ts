f057b228f14646daae39922189b468d69b9c5f94
dmitriy.evgn <129885619+nsnacademy@users.noreply.github.com>
2026-02-02 21:25:35 +0500

    v1.1 home

--- src/shared/types/telegram.d.ts
+++ src/shared/types/telegram.d.ts
@@ -4,15 +4,10 @@ declare global {
   interface TelegramWebApp {
     ready(): void;
     expand(): void;
-
-    // 🔹 FULLSCREEN (из официального API)
     requestFullscreen?: () => void;
     exitFullscreen?: () => void;
-
-    // 🔹 жесты
     disableVerticalSwipes?: () => void;
 
-    // 🔹 тема
     themeParams: {
       bg_color?: string;
       text_color?: string;
@@ -22,13 +17,10 @@ declare global {
       button_text_color?: string;
     };
 
-    // 🔹 пользователь
     initDataUnsafe?: {
       user?: {
         id: number;
         username?: string;
-        first_name?: string;
-        last_name?: string;
       };
     };
   }
