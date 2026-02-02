16b02ada48410dcc787ef8afa6f5a27e2a521848
dmitriy.evgn <129885619+nsnacademy@users.noreply.github.com>
2026-02-02 22:57:37 +0500

    v1.1 home

--- src/screens/Splash/styles.ts
+++ src/screens/Splash/styles.ts
@@ -1,30 +1,17 @@
 import styled from 'styled-components';
 
 export const SplashContainer = styled.div`
-<<<<<<< HEAD
-  flex: 1;
-=======
-  width: 100%;
-  height: 100%;
->>>>>>> e2fd922 (v1.1 home)
+  min-height: 100vh;
+  padding-top: env(safe-area-inset-top);
+  padding-bottom: env(safe-area-inset-bottom);
 
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
-<<<<<<< HEAD
-  padding: 16px;
-=======
-  /* 🔹 безопасные отступы уже учтены глобально */
-  padding: 16px;
-
->>>>>>> e2fd922 (v1.1 home)
-  text-align: center;
 `;
 
 export const Title = styled.h1`
   font-size: 32px;
-  margin: 0 0 8px;
-  font-weight: 700;
+  margin-bottom: 8px;
 `;
