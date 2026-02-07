ce73260124c024008aeaa7bd515de8851cfc9009
dmitriy.evgn <129885619+nsnacademy@users.noreply.github.com>
2026-02-07 21:07:48 +0500

    v1.8 фикс базы  мини плашка прогресса в хоме

--- src/screens/ChallengeDetails/index.tsx
+++ src/screens/ChallengeDetails/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { supabase } from '../../../shared/lib/supabase';
+import { supabase } from '../../shared/lib/supabase';
 
 import {
   SafeArea,
