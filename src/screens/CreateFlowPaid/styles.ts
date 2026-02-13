c0cbf0c2ec84a1834bf69b18890be63b6bc53154
dmitriy.evgn <129885619+nsnacademy@users.noreply.github.com>
2026-02-13 20:17:26 +0500

    7.0 платный вызов

--- src/screens/CreateFlowPaid/styles.ts
+++ src/screens/CreateFlowPaid/styles.ts
@@ -18,7 +18,7 @@ export const SafeArea = styled.div`
    HEADER
 ====================== */
 export const Header = styled.div`
-  padding: 50px 16px 12px;
+  padding: 90px 16px 12px;
   display: flex;
   flex-direction: column;
   gap: 6px;
@@ -503,7 +503,7 @@ export const ScrollContent = styled.div`
   flex-direction: column;
   gap: 12px;
   flex: 1;
-  margin-top: 120px; /* Высота фиксированного хедера */
+  margin-top: 170px; /* Высота фиксированного хедера */
   margin-bottom: 80px; /* Высота футера */
   overflow-y: auto;
   -webkit-overflow-scrolling: touch;
